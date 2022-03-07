cc10ef7198159e8ef0a5c70faea8963eb8829440
masnoonJunaid <masnoon20@gmail.com>
2022-03-07 21:44:33 +0530

    header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './headerStyle.css'
+const Header = () => {
+    return (
+        <header className='headerStyle'>
+            <h1 className='headerText'>Character of Thrones</h1>
+        </header>
+    )
+}
+
+export default Header
