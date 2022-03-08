2ad9c5dde3e9252d4d515e3479864b011f458e06
masnoonJunaid <masnoon20@gmail.com>
2022-03-08 21:49:24 +0530

    details component

---
+++ src/components/Details/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Details = (props) => {
+    return (
+        <Div>
+            <img src={props.img}/>
+            <ul>
+                <li>
+                    {props.fieldOne}
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+                <li>
+                </li>
+            </ul>
+        </Div>
+    )
+}
+
+export default Details
