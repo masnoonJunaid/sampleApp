5d67ad24b4c858a5dabbabb7ee7e8f2791f9497b
masnoonJunaid <masnoon20@gmail.com>
2022-03-08 21:50:53 +0530

    home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Card from "../Card"
+import { useState,useEffect } from "react"
+import "./homeStyle.css"
+
+const Home = () => {
+    const [isLoaded,setIsLoaded] = useState(false)
+    const [character,getCharacter] = useState([])
+    const [error,setError] = useState(null);
+
+    // Fetching character subset of data using useEeffect() hook to make sure,
+    // data is available before the DOM get rendered
+    useEffect(()=>{
+      fetch("https://www.anapioficeandfire.com/api/characters")
+          .then(res=>res.json())
+          .then(
+              (result) => {
+                  setIsLoaded(true);
+                  getCharacter(result);
+              },
+              (error)=>{
+                  setIsLoaded(false);
+                  setError(error)
+              }
+          )
+  },[])
+
+    if(error){
+        return <div>Something went  wrong!</div>
+    }else{
+        return (
+            <div className="homeContainer">
+                {character.map(card=>{
+                    return <Card cardData={card}/>
+                })}
+            </div>
+          );
+    }
+}
+export default Home
